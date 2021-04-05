bf3967b7640d0d96ba186cc05cd7b5082937fe62
Alena Buk <80640194+HelBuk@users.noreply.github.com>
2021-04-05 23:02:03 +0300

    Add files via upload
    Новая версия игры "Крестики-нолики" 1x
    Основной файл

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 class TicTacGame:
     table = list(range(1, 10))
-    cnt = 0 #счетчик ходов
+    cnt = 0  # счетчик ходов
+    r = 1    # показатель входа в цикл
+
     @staticmethod
     def show_board():
         print("_____________")
@@ -19,34 +21,31 @@ class TicTacGame:
             try:
                 n = int(n)
                 if 1 <= n <= 9:
-                    if (cls.table[n - 1] == 'x') | (cls.table[n - 1] == 'o'):
+                    if (TicTacGame.table[n - 1] == 'x') | (TicTacGame.table[n - 1] == 'o'):
                         print('Это место занято')
                         return cls.validate_input()
                     else:
-                     return n
+                        return n
                 else:
-                    print('Необходимо ввести целое число от 1 до 9')
+                    print('Необходимо ввести "Выход", если хотите закончить, или целое число от 1 до 9')
                     return cls.validate_input()
             except:
-                print('Необходимо ввести целое число от 1 до 9')
+                print('Необходимо ввести "Выход", если хотите закончить, или целое число от 1 до 9')
                 return cls.validate_input()
 
     @classmethod
     def start_game(cls):
-        while TicTacGame.r:
-            cls.show_board()
+        cls.show_board()
+        if TicTacGame.cnt % 2 == 0:
             print("Ход крестика")
             n = cls.validate_input()
-            cls.table[n - 1] = 'x'
-            cls.show_board()
-            TicTacGame.cnt += 1
-            cls.check_winner()
+            TicTacGame.table[n - 1] = 'x'
+
+        else:
             print("Ход нолика")
             n = cls.validate_input()
-            cls.table[n - 1] = 'o'
-            cls.show_board()
-            TicTacGame.cnt += 1
-            cls.check_winner()
+            TicTacGame.table[n - 1] = 'o'
+        cls.check_winner()
 
 
     @classmethod
@@ -54,23 +53,15 @@ class TicTacGame:
         res = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
         for each in res:
             if TicTacGame.table[each[0] - 1] == TicTacGame.table[each[1] - 1] == TicTacGame.table[each[2] - 1]:
-                print(TicTacGame.table[each[0] - 1] + ' is winning')
-                next = input('Чтобы начать новую игру введите "+": ')
-                if next=="+":
-                    print("Новая игра")
-                    TicTacGame.table = list(range(1, 10))
-                    return cls.start_game()
-                else:
-                    exit()
-            elif TicTacGame.cnt == 9:
+                #print(TicTacGame.table[each[0] - 1] + " is winning ")
+                return TicTacGame.table[each[0] - 1]
+            elif TicTacGame.cnt == 8:
                 print('Ничья')
-                next = input('Введите "-" если хотите закончить игру, нажмите на любую клавишу, чтобы продолжить игру')
-                if next == "-":
-                    exit()
-                else:
-                    print("Новая игра")
-                    TicTacGame.table = list(range(1, 10))
-                    return cls.start_game()
+                return 'Ничья'
+        TicTacGame.cnt += 1
+        print(TicTacGame.cnt)
+        return cls.start_game()
+
 
 
 if __name__ == '__main__':
